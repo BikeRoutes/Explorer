98e0599169e6306c3f77918abc0b510e9e5d065f
Francesco Cioria <firefelix@gmail.com>
2018-12-30 22:21:31 +0100

    update github page

---
+++ docs/4.bundle.b0d81f063569d83a518a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpwebclient=window.webpackJsonpwebclient||[]).push([[4],{785:function(e){e.exports={locales:["en"],messages:{"Hello.hello":"Hello {name}!","Hello.warningLength":"name length must be between 6 and 50"}}}}]);
+//# sourceMappingURL=4.bundle.b0d81f063569d83a518a.js.map
