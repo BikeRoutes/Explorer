d96be652276d26373712bd3ead535728c63c591e
Francesco Cioria <firefelix@gmail.com>
2018-12-30 16:08:30 +0100

    show layer popup automatically when mouse is close to layer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOMServer from "react-dom/server";
+import throttle = require("lodash/throttle");
 import { declareQueries } from "@buildo/bento/data";
 import { routes } from "queries";
 import { GeoJson } from "model";
@@ -16,6 +17,9 @@ type Feature = GeoJson["features"][number];
 class App extends React.Component<typeof queries.Props> {
   map: leaflet.Map;
   tileLayer: leaflet.Layer;
+  popup = leaflet.popup({
+    closeButton: false
+  });
 
   initializeMap() {
     // create map instance
@@ -52,11 +56,6 @@ class App extends React.Component<typeof queries.Props> {
 
       this.props.routes.value.map(route => {
         const layer = leaflet.geoJSON(route, {
-          onEachFeature: (feature: Feature, layer: any) => {
-            layer.bindPopup(
-              ReactDOMServer.renderToString(<Popup feature={feature} />)
-            );
-          },
           style: (feature: Feature) => ({
             color: feature.properties.color
           })
@@ -65,7 +64,53 @@ class App extends React.Component<typeof queries.Props> {
         this.map.addLayer(layer);
       });
     }
+
+    this.map.on("mousemove", this.onMouseMove);
   }
+
+  onMouseMove = throttle((e: leaflet.LeafletMouseEvent) => {
+    if (this.props.routes.ready) {
+      type ClosestRoute = {
+        distance: number;
+        point: leaflet.LatLng;
+        route: GeoJson;
+      };
+
+      const closestRoute: ClosestRoute = this.props.routes.value.reduce(
+        (acc, route) => {
+          const closestPoint: ClosestRoute = route.features[0].geometry.coordinates.reduce(
+            (acc, coordinates) => {
+              const point = leaflet.latLng(coordinates[1], coordinates[0]);
+              const distance = this.map
+                .latLngToContainerPoint(
+                  leaflet.latLng(coordinates[1], coordinates[0])
+                )
+                .distanceTo(this.map.latLngToContainerPoint(e.latlng));
+
+              return distance < acc.distance ? { distance, point, route } : acc;
+            },
+            { distance: Infinity } as ClosestRoute
+          );
+
+          return closestPoint.distance < acc.distance ? closestPoint : acc;
+        },
+        { distance: Infinity } as ClosestRoute
+      );
+
+      if (closestRoute.distance < 50) {
+        this.popup
+          .setLatLng(closestRoute.point)
+          .setContent(
+            ReactDOMServer.renderToString(
+              <Popup feature={closestRoute.route.features[0]} />
+            )
+          )
+          .openOn(this.map);
+      } else {
+        this.map.closePopup(this.popup);
+      }
+    }
+  }, 30);
 
   componentDidMount() {
     this.initializeMap();
