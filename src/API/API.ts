8af2a9f61b5aeab4e6f9b07aa8985446113803cd
Francesco Cioria <firefelix@gmail.com>
2019-01-09 02:01:59 +0100

    make getRoutes generic

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,39 +1,45 @@
 import { Content, GeoJSONFeatureCollection, GeoJSONFeature } from "model";
 import flatten = require("lodash/flatten");
 
+type FileContent = Content & { type: "file" };
+
+function getGeoJSONs(contents: Content[]): Promise<GeoJSONFeatureCollection[]> {
+  const folders = contents.filter(c => c.type === "dir");
+  if (folders.length > 0) {
+    return Promise.all(
+      folders.map(f => fetch(f.url).then(r => r.json()) as Promise<Content>)
+    )
+      .then(flatten)
+      .then(getGeoJSONs);
+  } else {
+    const files = contents.filter(
+      c => c.type === "file" && c.name.includes(".geojson")
+    ) as FileContent[];
+
+    return Promise.all(
+      files.map(f =>
+        fetch(f.download_url)
+          .then(r => r.json() as Promise<GeoJSONFeatureCollection>)
+          .then(x => ({
+            ...x,
+            features: [
+              {
+                ...x.features[0],
+                properties: {
+                  ...x.features[0].properties,
+                  url: f.html_url
+                }
+              }
+            ]
+          }))
+      )
+    );
+  }
+}
+
 export const getRoutes = (): Promise<GeoJSONFeature[]> => {
   return fetch("https://api.github.com/repos/BikeRoutes/Milano/contents")
     .then(res => res.json())
-    .then((contents: Content[]) => {
-      const folders = contents.filter(c => c.type === "dir");
-      return Promise.all(
-        folders.map(f =>
-          fetch(f.url).then(res => res.json() as Promise<Content[]>)
-        )
-      );
-    })
-    .then(flatten)
-    .then(contents =>
-      contents.filter(c => c.type === "file" && c.name.includes(".geojson"))
-    )
-    .then(contents => {
-      return Promise.all(
-        contents.map(c =>
-          fetch(c.download_url)
-            .then(r => r.json() as Promise<GeoJSONFeatureCollection>)
-            .then(route => {
-              const feature = route.features[0];
-              const geoJson: GeoJSONFeature = {
-                ...feature,
-                properties: {
-                  ...feature.properties,
-                  url: c.html_url
-                }
-              };
-
-              return geoJson;
-            })
-        )
-      );
-    });
+    .then(getGeoJSONs)
+    .then(geoJSONs => geoJSONs.map(route => route.features[0]));
 };
