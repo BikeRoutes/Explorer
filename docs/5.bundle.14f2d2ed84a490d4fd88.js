9d434426ee0cf9c296b9887e3d52a823f08ab51e
Francesco Cioria <firefelix@gmail.com>
2019-01-10 22:38:23 +0100

    update github page

---
+++ docs/5.bundle.14f2d2ed84a490d4fd88.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpwebclient=window.webpackJsonpwebclient||[]).push([[5],{966:function(e){e.exports={locales:["it"],messages:{"Hello.hello":"Ciao {name}!","Hello.warningLength":"la lunghezza del nome deve essere compresa tra 6 e 50"}}}}]);
+//# sourceMappingURL=5.bundle.14f2d2ed84a490d4fd88.js.map
