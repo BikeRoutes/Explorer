fa0e8547a41bf10d4f4eb8a88f60e06f0ed9983e
Francesco Cioria <firefelix@gmail.com>
2020-08-24 12:22:07 +0200

    remove useless button "exit navigation"

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -7,10 +7,8 @@ import { none, some, Option } from "fp-ts/lib/Option";
 import NoSleep from "nosleep.js";
 import { declareCommands } from "react-avenger";
 import { doUpdateLocation } from "../../commands";
-import { viewToLocation } from "../../model";
 import ElevationProfile from "../ElevationProfile";
-import * as mapboxgl from "mapbox-gl";
-import cx from "classnames";
+import mapboxgl from "mapbox-gl";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./navigation.scss";
@@ -114,33 +112,7 @@ class Navigation extends React.Component<Props, State> {
           );
 
           return (
-            <View
-              className={cx("navigation", { gpx: route.value.id === "gpx" })}
-              height="100%"
-              grow
-              column
-            >
-              <View
-                className="exit-navigation control-button"
-                onClick={() => {
-                  this.props.doUpdateLocation(
-                    viewToLocation({
-                      view: "details",
-                      routeId: some(route.value.id)
-                    })
-                  );
-                }}
-                hAlignContent="center"
-                vAlignContent="center"
-              >
-                <svg width="29" height="29" viewBox="0 0 29 29">
-                  <path
-                    fill="black"
-                    d="M18.5 16c-1.75 0-2.5.75-2.5 2.5V24h1l1.5-3 5.5 4 1-1-4-5.5 3-1.5v-1h-5.5zM13 18.5c0-1.75-.75-2.5-2.5-2.5H5v1l3 1.5L4 24l1 1 5.5-4 1.5 3h1v-5.5zm3-8c0 1.75.75 2.5 2.5 2.5H24v-1l-3-1.5L25 5l-1-1-5.5 4L17 5h-1v5.5zM10.5 13c1.75 0 2.5-.75 2.5-2.5V5h-1l-1.5 3L5 4 4 5l4 5.5L5 12v1h5.5z"
-                  ></path>
-                </svg>
-              </View>
-
+            <View className="navigation" height="100%" grow column>
               <View
                 className="toggle-elevation control-button"
                 onClick={() => {
