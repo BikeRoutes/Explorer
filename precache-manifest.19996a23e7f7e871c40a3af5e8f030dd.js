6eb381514491780fcce07166e9042e8c5b224c36
Francesco Cioria <firefelix@gmail.com>
2020-08-25 12:25:11 +0200

    Updates

---
+++ precache-manifest.19996a23e7f7e871c40a3af5e8f030dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd8c7f9a0292d047c509371b3f5e2eb",
+    "url": "/Explorer/index.html"
+  },
+  {
+    "revision": "d46779b9dbe71614df17",
+    "url": "/Explorer/static/css/2.fab1de8a.chunk.css"
+  },
+  {
+    "revision": "a3e8d5146d4b316ae82e",
+    "url": "/Explorer/static/css/main.ca35a144.chunk.css"
+  },
+  {
+    "revision": "d46779b9dbe71614df17",
+    "url": "/Explorer/static/js/2.5d680407.chunk.js"
+  },
+  {
+    "revision": "2287893861efca09d4c3a8cbe78fe6bd",
+    "url": "/Explorer/static/js/2.5d680407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e8d5146d4b316ae82e",
+    "url": "/Explorer/static/js/main.2d1c9e79.chunk.js"
+  },
+  {
+    "revision": "e15fd63fceca0598376b",
+    "url": "/Explorer/static/js/runtime-main.ad3862d6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/Explorer/precache-manifest.4dba956ba425452dd9570fded5b912a3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/Explorer/precache-manifest.19996a23e7f7e871c40a3af5e8f030dd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /** copy-pasted from original sw **/
 self.addEventListener("message", event => {
