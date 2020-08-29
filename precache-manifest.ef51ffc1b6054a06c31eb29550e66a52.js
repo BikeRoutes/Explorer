af284bad287b6a9da19f5b2be8d667b80247a33c
Francesco Cioria <firefelix@gmail.com>
2020-08-29 18:23:01 +0200

    Updates

---
+++ precache-manifest.ef51ffc1b6054a06c31eb29550e66a52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5339cf3433e3f56996dda9a45870a60",
+    "url": "/Explorer/index.html"
+  },
+  {
+    "revision": "443fd62e0291b637eb30",
+    "url": "/Explorer/static/css/2.fab1de8a.chunk.css"
+  },
+  {
+    "revision": "df0b13984e9d7e8be022",
+    "url": "/Explorer/static/css/main.aee5e668.chunk.css"
+  },
+  {
+    "revision": "443fd62e0291b637eb30",
+    "url": "/Explorer/static/js/2.7f3966d6.chunk.js"
+  },
+  {
+    "revision": "2287893861efca09d4c3a8cbe78fe6bd",
+    "url": "/Explorer/static/js/2.7f3966d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0b13984e9d7e8be022",
+    "url": "/Explorer/static/js/main.908d00d3.chunk.js"
+  },
+  {
+    "revision": "e15fd63fceca0598376b",
+    "url": "/Explorer/static/js/runtime-main.ad3862d6.js"
+  }
+]);
