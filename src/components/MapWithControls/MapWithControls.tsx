97af3f35f3a872e41b828b341bce29db8ce415a9
Francesco Cioria <firefelix@gmail.com>
2020-08-30 15:56:16 +0200

    compute heading instead of using plain alpha

--- src/components/MapWithControls/MapWithControls.tsx
+++ src/components/MapWithControls/MapWithControls.tsx
@@ -305,10 +305,51 @@ class MapWithControls extends React.Component<Props, State> {
     }
   };
 
+  compassHeading(alpha: number, beta: number, gamma: number): number {
+    // Convert degrees to radians
+    var alphaRad = alpha * (Math.PI / 180);
+    var betaRad = beta * (Math.PI / 180);
+    var gammaRad = gamma * (Math.PI / 180);
+
+    // Calculate equation components
+    var cA = Math.cos(alphaRad);
+    var sA = Math.sin(alphaRad);
+    var cB = Math.cos(betaRad);
+    var sB = Math.sin(betaRad);
+    var cG = Math.cos(gammaRad);
+    var sG = Math.sin(gammaRad);
+
+    // Calculate A, B, C rotation components
+    var rA = -cA * sG - sA * sB * cG;
+    var rB = -sA * sG + cA * sB * cG;
+    var rC = -cB * cG;
+
+    // Calculate compass heading
+    var compassHeading = Math.atan(rA / rB);
+
+    // Convert from half unit circle to whole unit circle
+    if (rB < 0) {
+      compassHeading += Math.PI;
+    } else if (rA < 0) {
+      compassHeading += 2 * Math.PI;
+    }
+
+    // Convert radians to degrees
+    compassHeading *= 180 / Math.PI;
+
+    return compassHeading;
+  }
+
   onDeviceOrientation = throttle((e: DeviceOrientationEvent) => {
     this.setState(
       {
-        deviceBearing: fromNullable(e.alpha).map(alpha => -alpha) // bearing is the opposite of alpha
+        deviceBearing: fromNullable(e.alpha).chain(alpha =>
+          fromNullable(e.beta).chain(beta =>
+            fromNullable(e.gamma).map(gamma =>
+              this.compassHeading(alpha, beta, gamma)
+            )
+          )
+        )
       },
       this.rotateUserLocationDot
     );
